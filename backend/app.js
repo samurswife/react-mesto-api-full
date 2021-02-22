2724759e217dfc8fafda3c68c970b5888c616ce5
Marina Shakarova <marina-seed@yandex.ru>
2021-02-22 14:51:29 +0700

    ref: раскомментировала опцию корс

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const options = {
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
-  // optionsSuccessStatus: 204,
+  optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
