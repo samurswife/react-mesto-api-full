fbf623cde22de1a2d5e124a2dbaf8244da904b9d
Marina Shakarova <marina-seed@yandex.ru>
2021-02-28 18:49:35 +0700

    tech commit

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -31,7 +31,7 @@ const deleteCard = (req, res, next) => {
 
 const likeCard = (req, res, next) => {
   const { cardId } = req.params;
-  const user = req.user._id;
+  const { user } = req.user._id;
 
   Card.findByIdAndUpdate(
     cardId,
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -115,8 +115,6 @@ function App() {
 
   function handleCardLike(card) {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
-    console.log(token, api, currentUser, isLiked);
-    debugger;
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
         const newCards = cards.map((c) => c._id === card._id ? newCard : c);
